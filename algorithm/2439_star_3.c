50789997185d613b70d7cb28ee8a2c0f8f8596f7
DanielLee <31802525+InfiniteRegen@users.noreply.github.com>
2018-05-23 21:18:41 +0900

    2440.c_[PrintOut_Stars_3]

---
+++ algorithm/2439_star_3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	int N = 0;
+
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			if ((i + j) < N ) {
+				printf("*");
+			}
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
