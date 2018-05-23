9641398cdab8424722a5115f1d7387b359dac0ac
DanielLee <31802525+InfiniteRegen@users.noreply.github.com>
2018-05-23 21:44:35 +0900

    1924.c_[2007-calendar]

---
+++ algorithm/1924.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+static char wordSet[7][4] = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+static int daySet[12] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30};
+
+int main(int argc, char** argv)
+{
+	int mon = 0, day = 0;
+	int daysGone = 0;
+
+	scanf("%d %d", &mon, &day);
+
+	for (int i = 0; i < mon; ++i) {
+		daysGone += daySet[i];
+	}
+	daysGone += (day - 1);
+
+	printf("%s", wordSet[daysGone % 7]);
+
+	return 0;
+}
